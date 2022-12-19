3be17b45f56f95d2a294da2654fa7c40de56e746
Shitov Ivan <solnishko313131@gmail.com>
2022-12-19 14:36:24 +0500

    Web Socket STOMP Chat

---
+++ src/main/java/com/webmessenger/chat/model/ChatMessage.java
@@ -0,0 +1,46 @@
+package com.webmessenger.chat.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ChatMessage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private MessageType type;
+    private String content;
+    private String sender;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public void setType(MessageType type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+}
---
+++ src/test/java/com/webmessenger/chat/WebsocketDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webmessenger.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WebsocketDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
